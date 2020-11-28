e05c0a5c5bbd39f03721d5b5831a54823d119351
shuji ochi <ponpoko1968@gmail.com>
2020-11-28 12:50:19 +0900

    Initial Commit

---
+++ FirebaseStorageExample/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  FirebaseStorageExample
+//
+//  Created by 越智修司 on 2020/11/28.
+//  Copyright © 2020 shuji ochi. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        configureView()
+    }
+
+    var detailItem: NSDate? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
